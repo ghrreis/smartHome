56a4752fc4969b324edab5a1f1a60ff85cdb79b6
Gustavo Henrique da Rocha Reis <gustavo.reis@ifsudestemg.edu.br>
2022-11-20 20:31:48 -0300

    Classe para verificar autenticação e níveis de acesso

---
+++ Security.py
@@ -0,0 +1,26 @@
+# Classe para verificação de autenticação e níveis de acesso
+
+import json
+
+
+class Login():
+
+    __user = ""
+    __password = ""
+    __status = 0
+
+    def __init__(self, username, password):
+        self.__user = username
+        self.__password = password
+        self.__status = 0
+        f = open("login.json", "r")
+        file = f.read()
+        users = json.loads(file)
+        print(users)
+        for login in users:
+            if (self.__user == login["username"]) and (self.__password == login["password"]):
+                self.__status = 1
+                break
+
+    def status(self):
+        return self.__status
